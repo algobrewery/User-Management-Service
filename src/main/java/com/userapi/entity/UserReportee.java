c839e2c1f67abdc90c7e1125ec8e22899b29249a
sindhu-jinukala <sindhuj@algobrewery.com>
2025-04-25 16:37:27 +0530

    Create UserReportee.java

---
+++ src/main/java/com/userapi/entity/UserReportee.java
@@ -0,0 +1,65 @@
+package com.userapi.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_reportees")
+public class UserReportee {
+
+    @Id
+    @Column(name = "relation_uuid")
+    private String relationUuid;
+
+    @Column(name = "organization_uuid", nullable = false)
+    private String organizationUuid;
+
+    @Column(name = "manager_user_uuid", nullable = false)
+    private String managerUserUuid;
+
+    @Column(name = "user_uuid", nullable = false)
+    private String userUuid;
+
+    @Column(name = "job_profile_uuid", nullable = false)
+    private String jobProfileUuid;
+
+    // Getters and Setters
+    public String getRelationUuid() {
+        return relationUuid;
+    }
+
+    public void setRelationUuid(String relationUuid) {
+        this.relationUuid = relationUuid;
+    }
+
+    public String getOrganizationUuid() {
+        return organizationUuid;
+    }
+
+    public void setOrganizationUuid(String organizationUuid) {
+        this.organizationUuid = organizationUuid;
+    }
+
+    public String getManagerUserUuid() {
+        return managerUserUuid;
+    }
+
+    public void setManagerUserUuid(String managerUserUuid) {
+        this.managerUserUuid = managerUserUuid;
+    }
+
+    public String getUserUuid() {
+        return userUuid;
+    }
+
+    public void setUserUuid(String userUuid) {
+        this.userUuid = userUuid;
+    }
+
+    public String getJobProfileUuid() {
+        return jobProfileUuid;
+    }
+
+    public void setJobProfileUuid(String jobProfileUuid) {
+        this.jobProfileUuid = jobProfileUuid;
+    }
+}
