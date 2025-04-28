f1f49fc413d0eab8bfa5995e45d9768585a251be
sindhu-jinukala <sindhuj@algobrewery.com>
2025-04-28 14:44:54 +0530

    Update EmailInfo.java

--- src/main/java/com/userapi/models/external/EmailInfo.java
+++ src/main/java/com/userapi/models/external/EmailInfo.java
@@ -6,13 +6,17 @@ import lombok.Setter;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
-@Getter
-@Setter
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
 public class EmailInfo {
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;
 
+    @NotBlank(message = "Verification status is required")
     private String verificationStatus;
-
 }
