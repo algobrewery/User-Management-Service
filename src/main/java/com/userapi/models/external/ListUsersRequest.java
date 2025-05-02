4d4449bd61902fd41a49f6f7d44c2d22c460b619
sindhu-jinukala <sindhuj@algobrewery.com>
2025-05-02 08:56:07 +0530

    Update ListUsersRequest.java

--- src/main/java/com/userapi/models/external/ListUsersRequest.java
+++ src/main/java/com/userapi/models/external/ListUsersRequest.java
@@ -1,9 +1,21 @@
 package com.userapi.models.external;
 
 import lombok.Data;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 
 @Data
 public class ListUsersRequest {
+    @Valid
     private ListUsersFilterCriteria filterCriteria;
     private ListUsersSelector selector;
+
+    @Min(value = 0, message = "Page number must be greater than or equal to 0")
+    private Integer page = 0;
+
+    @Min(value = 1, message = "Page size must be greater than 0")
+    private Integer size = 10;
+
+    private String sortBy = "username";
+    private String sortDirection = "asc";
 }
