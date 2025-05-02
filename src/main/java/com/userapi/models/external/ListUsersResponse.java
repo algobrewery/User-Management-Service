86b6db10d1c76f659274e74db3043ea9645694a3
sindhu-jinukala <sindhuj@algobrewery.com>
2025-05-02 08:56:34 +0530

    Update ListUsersResponse.java

--- src/main/java/com/userapi/models/external/ListUsersResponse.java
+++ src/main/java/com/userapi/models/external/ListUsersResponse.java
@@ -1,12 +1,16 @@
 package com.userapi.models.external;
 
 import lombok.Data;
-
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class ListUsersResponse {
     private List<Map<String, Object>> users;
+    private long totalElements;
+    private int totalPages;
+    private int currentPage;
+    private int pageSize;
+    private boolean hasNext;
+    private boolean hasPrevious;
 }
-
