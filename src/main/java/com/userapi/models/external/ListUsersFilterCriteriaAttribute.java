3dfcda8b7cced194d8fe625b51fa033f9c120a4c
sindhu-jinukala <sindhuj@algobrewery.com>
2025-05-02 08:55:43 +0530

    Update ListUsersFilterCriteriaAttribute.java

--- src/main/java/com/userapi/models/external/ListUsersFilterCriteriaAttribute.java
+++ src/main/java/com/userapi/models/external/ListUsersFilterCriteriaAttribute.java
@@ -1,11 +1,15 @@
 package com.userapi.models.external;
 
 import lombok.Data;
-
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class ListUsersFilterCriteriaAttribute {
+    @NotBlank(message = "Attribute name is required")
     private String name;
+
+    @NotNull(message = "Attribute values are required")
     private List<String> values;
 }
