6750ae83a75c0a6a295c994bf49044dbe71c7d65
sindhu-jinukala <sindhuj@algobrewery.com>
2025-05-05 10:45:40 +0530

    Update UserServiceImpl.java

--- src/main/java/com/userapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/userapi/service/impl/UserServiceImpl.java
@@ -29,15 +29,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.userapi.models.external.UpdateUserRequest;
+import com.userapi.models.external.UpdateUserResponse;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
