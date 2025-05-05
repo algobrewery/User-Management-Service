068276f338befff62ce753247e53b4728dfbda76
sindhu-jinukala <sindhuj@algobrewery.com>
2025-05-05 09:24:45 +0530

    Implemented updateUser method in UserService

--- src/main/java/com/userapi/service/UserService.java
+++ src/main/java/com/userapi/service/UserService.java
@@ -10,4 +10,7 @@ public interface UserService {
     CreateUserInternalResponse createUser(CreateUserInternalRequest request);
     GetUserResponse getUser(String orgUUID, String userId);
     ListUsersResponse listUsers(ListUsersRequest request, String orgUuid);
+    UpdateUserResponse updateUser(String orgUuid, String userId, UpdateUserRequest request);
+    UpdateUserResponse deactivateUser(String orgUuid, String userId);
+
 }
