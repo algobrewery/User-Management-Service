df62491b480af0918e421bf9ee6fb5cb18ee63c0
sindhu-jinukala <sindhuj@algobrewery.com>
2025-05-12 10:39:40 +0530

    Update UserListController.java

--- src/main/java/com/userapi/controller/UserListController.java
+++ src/main/java/com/userapi/controller/UserListController.java
@@ -5,6 +5,8 @@ import com.userapi.models.external.ListUsersResponse;
 import com.userapi.models.external.UserHierarchyResponse;
 import com.userapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,8 @@ import static com.userapi.common.constants.HeaderConstants.*;
 @RequestMapping("/users") // Base path for multiple users operations
 @RequiredArgsConstructor
 public class UserListController {
+    private static final Logger logger = LoggerFactory.getLogger(UserListController.class);
+
 
     private final UserService userService;
 
@@ -29,16 +33,23 @@ public class UserListController {
             @RequestHeader(APP_REGION_ID) String regionId,
             @Valid @RequestBody ListUsersRequest request) {
 
+        logger.info("Listing users for org: {}, traceId: {}, page: {}, size: {}",
+                orgUuid, traceId, request.getPage(), request.getSize());
+
         ListUsersResponse response = userService.listUsers(request, orgUuid);
+        logger.info("Found {} users for org: {}", response.getTotalElements(), orgUuid);
         return ResponseEntity.ok(response);
     }
+
     @GetMapping("/{userId}/hierarchy")
     public ResponseEntity<UserHierarchyResponse> getUserHierarchy(
             @RequestHeader(APP_ORG_UUID) String orgUUID,
             @PathVariable String userId) {
 
+        logger.info("Fetching user hierarchy for user: {} in org: {}", userId, orgUUID);
+
         UserHierarchyResponse response = userService.getUserHierarchy(orgUUID, userId);
+        logger.info("User hierarchy fetched successfully for user: {}", userId);
         return ResponseEntity.ok(response);
     }
-
 }
